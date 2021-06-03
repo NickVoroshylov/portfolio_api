bba1c489c5bde79b1a3e99a36515c12cdcfe701a
NickVoroshylov <nikita.voroshylov@gmail.com>
2021-06-03 10:16:46 +0300

    Add .gitignore file

--- portfolio_api/urls.py
+++ portfolio_api/urls.py
@@ -1,4 +1,4 @@
- """portfolio_api URL Configuration
+"""portfolio_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
