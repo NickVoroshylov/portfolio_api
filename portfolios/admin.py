941f47f76c4ffd27f21653b0cc4d1284cee6d51c
NickVoroshylov <nikita.voroshylov@gmail.com>
2021-06-03 10:44:22 +0300

    Displayed Picture model in admin

--- portfolios/admin.py
+++ portfolios/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Portfolio
+from .models import Portfolio, Picture
 
 
 class PortfolioAdmin(admin.ModelAdmin):
@@ -8,4 +8,10 @@ class PortfolioAdmin(admin.ModelAdmin):
     list_display = ('owner', 'name', 'created')
 
 
+class PictureAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'description')
+    list_display = ('portfolio', 'name', 'created')
+
+
 admin.site.register(Portfolio, PortfolioAdmin)
+admin.site.register(Picture, PictureAdmin)
