e7d4a7034b8850c85c5771805f739f9ce2714de9
NickVoroshylov <nikita.voroshylov@gmail.com>
2021-06-03 10:43:24 +0300

    Displayed Portfolio model in admin

--- portfolios/admin.py
+++ portfolios/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Portfolio
+
+
+class PortfolioAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'description')
+    list_display = ('owner', 'name', 'created')
+
+
+admin.site.register(Portfolio, PortfolioAdmin)
