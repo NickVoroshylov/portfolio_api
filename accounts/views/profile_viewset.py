029c7346b3f78693c2591643a15d67b6b70f8433
NickVoroshylov <nikita.voroshylov@gmail.com>
2021-06-04 12:31:33 +0300

    Create Profile ViewSet

--- accounts/views/profile_viewset.py
+++ accounts/views/profile_viewset.py
@@ -1,9 +1,14 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import ListModelMixin, UpdateModelMixin, RetrieveModelMixin
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 
 from accounts.models import Profile
 from accounts.serializers import ProfileSerializer
 
 
-class ProfileViewSet(ModelViewSet):
-    queryset = Profile.objects.all()
+class ProfileViewSet(RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Profile.objects.filter(user__username=self.request.user.username)
