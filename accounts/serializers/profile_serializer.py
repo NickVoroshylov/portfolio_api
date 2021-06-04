f5b7d00ceda24ca654e5428a92ec7d322c58f5f0
NickVoroshylov <nikita.voroshylov@gmail.com>
2021-06-04 11:07:46 +0300

    Create serializer for Profile model

---
+++ accounts/serializers/profile_serializer.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+
+from accounts.models import Profile
+
+
+class ProfileSerializer(ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('user', 'avatar', 'bio')
