4228a0d13c8a43b13fd43dcd1a98d5f8f5ed00d2
NickVoroshylov <nikita.voroshylov@gmail.com>
2021-06-04 13:02:56 +0300

    Add picture field to Picture model

--- portfolios/models/picture.py
+++ portfolios/models/picture.py
@@ -3,10 +3,16 @@ from django.db import models
 from .portfolio import Portfolio
 
 
+def portfolio_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/portfolio_owner - portfolio_name/<filename>
+    return f"portfolios/{instance.portfolio.owner} - {instance.portfolio.name}/{filename}"
+
+
 class Picture(models.Model):
     portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to=portfolio_directory_path)
     name = models.CharField(max_length=255)
-    description = models.TextField(max_length=500)
+    description = models.TextField(max_length=500, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
